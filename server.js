325c2e700f66ce12ad072dd3c9340188f80b0300
Samantha Malone <sammac99@gmail.com>
2021-07-12 16:54:36 -0700

    added to mysql

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection(
       // Your MySQL username,
       user: 'root',
       // Your MySQL password
-      password: '',
+      password: 'Lewewe1!',
       database: 'election'
     },
     console.log('Connected to the election database.')
