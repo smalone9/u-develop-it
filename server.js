e0099894ed06f52cf7031ee2b651b752ca60d5b8
Samantha Malone <sammac99@gmail.com>
2021-07-13 10:07:53 -0700

    added query for create operation

--- server.js
+++ server.js
@@ -33,13 +33,25 @@ db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
   console.log(row);
 });
 
-// Delete a candidate
-db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
   if (err) {
     console.log(err);
   }
   console.log(result);
 });
+
+// // Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
