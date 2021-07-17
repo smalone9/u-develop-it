f5f84879e3d18cb4b62a114a5ecbbba3e35461de
Samantha Malone <sammac99@gmail.com>
2021-07-17 13:41:00 -0700

    modularized database connection

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const mysql = require('mysql2');
-const inputCheck = require('./utils/inputCheck');
+const db = require('./db/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,19 +7,6 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// Connect to database
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // Your MySQL username,
-      user: 'root',
-      // Your MySQL password
-      password: 'root',
-      database: 'election'
-    },
-    console.log('Connected to the election database.')
-  );
 
   // Get all candidates
 app.get('/api/candidates', (req, res) => {
